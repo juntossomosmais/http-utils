0480cd13840db5b4e6fe5e5ae0e1a92abf8a44c5
Alexandre Xavier <ale.bxsantos@gmail.com>
2021-05-03 09:30:16 -0300

    feat: added a optional hooks parameter to request session

--- http_utils/session.py
+++ http_utils/session.py
@@ -5,6 +5,7 @@ import functools
 
 from contextlib import contextmanager
 from typing import Any
+from typing import Callable
 from typing import FrozenSet
 from typing import Generator
 from typing import List
@@ -31,21 +32,26 @@ def request_session(
     backoff_factor: float = HTTP_BACKOFF_FACTOR,
     status_forcelist: Optional[Tuple] = HTTP_STATUS_FORCELIST,
     allowed_methods: Union[FrozenSet] = Retry.DEFAULT_ALLOWED_METHODS,
+    hooks: Optional[List[Callable]] = None,
     **kwargs: Any,
 ) -> Generator[Session, None, None]:
     """
     Generate a requests session that allows retries and raises HTTP errors (status code >= 400).
     Uses the same arguments as the class Retry from urllib3
     """
-    # creates new requests session
+
     session = Session()
+    session.request = functools.partial(session.request, timeout=HTTP_TIMEOUT)
+
+    # Adds the default log_response hook if no hooks are provided.
+    if hooks is None:
+        hooks = [log_response]
     # Adds the [allowed_http_error_status_list] to the check for errors hook.
     functools.partial(
         check_for_errors,
         allowed_http_error_status_list=allowed_http_error_status_list,
     )
-    session.request = functools.partial(session.request, timeout=HTTP_TIMEOUT)
-    session.hooks.update(response=[log_response, check_for_errors])
+    session.hooks.update(response=[*hooks, check_for_errors])
 
     adapter = HTTPAdapter(
         max_retries=Retry(
